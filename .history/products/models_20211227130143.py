00f97e85f654f93136aa04cd862b0deb464c77d7
Mark Smyth <marks530@gmail.com>
2021-12-27 14:50:01 +0000

    Added product app models and fixtures

--- .history/products/models_20211227130143.py
+++ .history/products/models_20211227130143.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=254)
-    supplier = models.CharField(max_length=254, null=True, blank=True)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
 
-    def get_supplier(self):
-        return self.supplier
+    def get_friendly_name(self):
+        return self.friendly_name
 
 
 class Product(models.Model):
