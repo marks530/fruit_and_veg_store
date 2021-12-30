c9b4c197a92f2deac0008b9f105bb614b5bb6725
Mark Smyth <marks530@gmail.com>
2021-12-30 11:27:09 +0000

    Added add to bag functionality

--- .history/bag/views_20211230112234.py
+++ .history/bag/views_20211230112234.py
@@ -19,5 +19,5 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
+    print(request.session)
     return redirect(redirect_url)
