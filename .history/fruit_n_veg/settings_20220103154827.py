a9698f2fd096725912eb656546b79f175842bd30
Mark Smyth <marks530@gmail.com>
2022-01-03 15:57:39 +0000

    Added checkout forms admin and signals

--- .history/fruit_n_veg/settings_20220103154827.py
+++ .history/fruit_n_veg/settings_20220103154827.py
@@ -56,7 +56,7 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 
     # `allauth` specific authentication methods, such as login by e-mail
-    'allauth.account.auth_backends.AuthenticationBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',  
 )
 
 SITE_ID = 1
