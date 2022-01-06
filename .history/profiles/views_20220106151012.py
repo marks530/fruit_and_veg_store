aa922e7c2e2fec61dc500d8ec807047ddc18c576
Mark Smyth <marks530@gmail.com>
2022-01-06 15:44:16 +0000

    Update profile template

--- .history/profiles/views_20220106151012.py
+++ .history/profiles/views_20220106151012.py
@@ -6,14 +6,12 @@ from .forms import UserProfileForm
 
 def profile(request):
     """ Display the user's profile. """
-    
-    profile = get_object_or_404(UserProfile, user=request.user )
+
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
     
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
         'form': form,
         'orders': orders
     }
--- .history/profiles/views_20220106153822.py
+++ .history/profiles/views_20220106153822.py
@@ -6,8 +6,7 @@ from .forms import UserProfileForm
 
 def profile(request):
     """ Display the user's profile. """
-    
-    profile = get_object_or_404(UserProfile, user=request.user )
+
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
     
