8909ed36bd14c03164272583d16e8310ab83768f
Mark Smyth <marks530@gmail.com>
2022-01-06 12:19:54 +0000

    Updated checkout form

--- .history/checkout/models_20220106120801.py
+++ .history/checkout/models_20220106120801.py
@@ -6,7 +6,7 @@ import uuid
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
-from django_countries.fields import CountryField
+from django_countries import CountryField
 
 from products.models import Product
 
