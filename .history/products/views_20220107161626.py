993f9f670f471f439e307de0cd36a1829bfda677
Mark Smyth <marks530@gmail.com>
2022-01-07 16:28:12 +0000

    Add product delete in Product Admin

--- .history/products/views_20220107161626.py
+++ .history/products/views_20220107161626.py
@@ -72,9 +72,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            product = form.save()
+            form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('product_detail', args=[product.id]))
+            return redirect(reverse('add_product'))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:   
