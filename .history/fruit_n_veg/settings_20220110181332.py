234ca8bb01c7f6c28e70dfe32cd16d1683cc8fff
Mark Smyth <marks530@gmail.com>
2022-01-10 18:16:55 +0000

    Removed secret key and set debug

--- .history/fruit_n_veg/settings_20220110181332.py
+++ .history/fruit_n_veg/settings_20220110181332.py
@@ -25,10 +25,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = '^l(vjktijz7_m)*3^gwbqu*%hu$mcrm71es9a3(_!vsf&05f*b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['marks530-fruitnveg1.herokuapp.com', 'localhost', '127.0.0.1']
