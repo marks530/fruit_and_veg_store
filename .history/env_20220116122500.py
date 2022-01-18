f6c300c49dba65d9383fbff38de62143b8179520
Mark Smyth <marks530@gmail.com>
2022-01-18 15:23:26 +0000

    Update order history and fix display of product weight

---
+++ .history/env_20220116122500.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DEVELOPMENT"] = "False"
+os.environ["SECRET_KEY"] = '^l(vjktijz7_m)*3^gwbqu*%hu$mcrm71es9a3(_!vsf&05f*b'
+os.environ['STRIPE_SECRET_KEY'] = 'sk_test_51KECn6G43AyNYmuKlACutC4wMSFunqV23VX94lK5SY5YLvVAjoRYAToCsxMvEBtMq7FDIPxdUfrX2gqjMF9jBZTg00OP2idHfS'
---
+++ .history/env_20220116122657.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ["DEVELOPMENT"] = "False"
+os.environ["SECRET_KEY"] = '^l(vjktijz7_m)*3^gwbqu*%hu$mcrm71es9a3(_!vsf&05f*b'
+os.environ['STRIPE_SECRET_KEY'] = 'sk_test_51KECn6G43AyNYmuKlACutC4wMSFunqV23VX94lK5SY5YLvVAjoRYAToCsxMvEBtMq7FDIPxdUfrX2gqjMF9jBZTg00OP2idHfS'
+os.environ['STRIPE_PUBLIC_KEY'] = 'pk_test_51KECn6G43AyNYmuKX52rCpG6GuqWWH8VTUDSi96bhsg0BA9e1BUagFUo4GLbyUzUYZ1jMlNFUycN2axp8PYw6WYj00RNlNUU6Y'
